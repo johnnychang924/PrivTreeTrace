2816651ca402ad2226d42c315d79fba65126aab7
johnnychang924 <johnny611247@gmail.com>
2025-06-13 21:45:37 +0800

    first commit

---
+++ dataset_convertor.py
@@ -0,0 +1,29 @@
+import glob
+import os
+
+folder_path = "/home/johnnychang/下載/Geolife Trajectories 1.3/Data"
+output_trace = "./datasets/simple_example.dat"
+max_data_per_people = 600
+
+with open(output_trace, "w") as output:
+    for person_id in range(182):
+        output.write(f"#{person_id}:\n>0:")
+        write_out_data = 0
+        person_folder = os.path.join(folder_path, str(person_id).zfill(3), "Trajectory")
+        for file_path in glob.glob(os.path.join(person_folder, "*"), recursive=True):
+            if not file_path.endswith(".plt"):
+                continue
+            with open(file_path, "r") as person_input:
+                for _ in range(6):
+                    person_input.readline()
+                for row in person_input:
+                    splited_row = row.split(",")
+                    output.write(f"{splited_row[0]},{splited_row[1]};")
+                    write_out_data = write_out_data + 1
+                    if write_out_data > max_data_per_people:
+                        break
+                if write_out_data > max_data_per_people:
+                    break
+        output.write("\n")
+                
+    
